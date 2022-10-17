ceadb6645e472f0e70e4be2e56fa0f1015524618
Louis <louyoon89@gmail.com>
2022-10-17 13:39:18 -0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# WEEK-3-Challenge
+# Week-3-Challenge-Random-Password-Generator
+Week 3 Challenge - Random Password Generator Project
+
+## Random Password Generator
+* Week 3's Challenge requires us to modify a starter code to create an application powered by Javascript that enables users to create a random password based on desired criteria. 
+### Challenge Goals
+* Present series of prompts for password critieria:
+    * Uppercase, Lowercase, Numeric, and/or Special
+    * Length of password
+    * All selected critierias generated
+* Generates password on page
+* Once code base is complete we will have to deploy the application to a live URL and Github URL with new repository that satisfies repository criterias.
+
+
+## Screenshot
+1. Prompt when "Generate" selected
+![Screenshot of application](./Assets/SC1.png)
+2. Series of Prompts to collect password criteria
+![Screenshot of application](./Assets/SC2.png)
+3. Error screen (return back to question if proper value not provided)
+![Screenshot of application](./Assets/SC3.png)
+
+
+
+## Status
+*  Variables/Functions/Values/Parameters created to provide data.
+*  Variables/Functions/Values/Parameters created to collect criteria data.
+*  Functions created to gather all data through prompts.
+*  Conditions provided and randomized to produce password.
+*  URLs live
+
+### Live Link
+* Your site is live at 
+https://louyoon89.github.io/Week-3-Challenge-Random-Password-Generator/
+Last deployed by @github-pages github-pages
+### Github Link
+* https://github.com/louyoon89/Week-3-Challenge-Random-Password-Generator
---
+++ script.js
@@ -0,0 +1,154 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Variables (values for)
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberValue = "0123456789";
+var specialValue = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+//Variables (for functions,etc)
+var pwLength;
+var pwUpper;
+// var pwLower; - var auto in function for pw gen
+var pwNumber;
+var pwSpecial;
+
+//Functions:
+//Length
+//NaN for not #
+function createLength() {
+  pwLength = prompt(
+    "Please enter a NUMBER between 8-128 characters to determine your Password's length: "
+  );
+
+  if (pwLength < 8) {
+    alert(
+      "Please enter a NUMBER between 8-128 characters to determine your Password's length: "
+    );
+    createLength();
+  } else if (pwLength > 128) {
+    alert(
+      "Please enter a NUMBER between 8-128 characters to determine your Password's length: "
+    );
+    createLength();
+  } else if (isNaN(pwLength)) {
+    alert(
+      "Please enter a NUMBER between 8-128 characters to determine your Password's length: "
+    );
+    createLength();
+    return pwLength;
+  }
+}
+
+//Uppercase
+function createUpper() {
+  pwUpper = prompt("Add UPPERCASE letters to your password?");
+  if (pwUpper == "Yes" || pwUpper == "yes") {
+    pwUpper = true;
+    return pwUpper;
+  } else if (pwUpper == "No" || pwUpper == "no") {
+    pwUpper = false;
+    return pwUpper;
+  } else {
+    alert("UPPERCASE letters? : Yes or No");
+    createUpper();
+  }
+  return pwUpper;
+}
+
+// Lowercase // CREATE AUTO VARIATION
+// function createLower() {
+//   pwLower = prompt("Add LOWERCASE letters to your password?");
+//   if (pwLower == "Yes" || pwLower == "yes") {
+//     pwLower = true;
+//     return pwLower;
+//   } else if (pwLower == "No" || pwLower == "no") {
+//     pwLower = false;
+//     return pwLower;
+//   } else {
+//     alert("LOWERCASE letters? : Yes or No");
+//     createLower();
+//   }
+//   return pwLower;
+// }
+
+//Numbers
+function createNumbers() {
+  pwNumber = prompt("Add NUMBERS to your password?");
+  if (pwNumber == "Yes" || pwNumber == "yes") {
+    pwNumber = true;
+    return pwNumber;
+  } else if (pwNumber == "No" || pwNumber == "no") {
+    pwNumber = false;
+    return pwNumber;
+  } else {
+    alert("Numbers? : Yes or No");
+    createNumbers();
+  }
+  return pwNumber;
+}
+
+//Special
+function createSpecial() {
+  pwSpecial = prompt("Add SPECIAL characters to your password?");
+  if (pwSpecial == "Yes" || pwSpecial == "yes") {
+    pwSpecial = true;
+    return pwSpecial;
+  } else if (pwSpecial == "No" || pwSpecial == "no") {
+    pwSpecial = false;
+    return pwSpecial;
+  } else {
+    alert("Special Characters? : Yes or No");
+    createSpecial();
+  }
+  return pwSpecial;
+}
+
+//Run Previous Function Info
+function generatePassword() {
+  createLength();
+  console.log(pwLength);
+  createUpper();
+  console.log(pwUpper);
+  createNumbers();
+  console.log(pwNumber);
+  createSpecial();
+  console.log(pwSpecial);
+  //CONDITIONS FOR RANDOM GEN
+  //MATH RANDOM FOR AUTO GEN
+  //CREATE VAR AUTO TO REDUCE,LOWER AS DEFAULT *** auto+=
+  var auto = lowerCase;
+  var password = "";
+  if (pwUpper && pwNumber && pwSpecial) {
+    auto += upperCase + numberValue + specialValue;
+  } else if (pwUpper && pwNumber) {
+    auto += upperCase + numberValue;
+  } else if (pwUpper && pwSpecial) {
+    auto += upperCase + specialValue;
+  } else if (numberCheck && pwSpecial) {
+    auto += numberValue + specialValue;
+  } else if (pwUpper) {
+    auto += upperCase;
+  } else if (pwNumber) {
+    auto += numberValue;
+  } else if (pwSpecial) {
+    auto += specialValue;
+  } else {
+    auto = lowerCase;
+  }
+  for (var i = 0; i < pwLength; i++) {
+    password += auto.charAt(Math.floor(Math.random() * auto.length));
+  }
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
